adde0abdc80d9c4657f7bcb12b276a470decec69
AlexGeorgeLain <alex.george.lain@gmail.com>
2021-07-16 22:14:03 +0100

    update
    download error fixed and comments added.

--- ssdteam/models.py
+++ ssdteam/models.py
@@ -17,7 +17,7 @@ from ssdteam import db, login_manager
 
 @login_manager.user_loader
 def load_user(user_id):
-    """Loads user as current_user
+    """Loads user as current_user.
 
     Keyword args:
         user_id -- id of logged in user.
@@ -29,6 +29,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     """User table in database. Stores user information"""
 
+    # database columns.
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
@@ -36,6 +37,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable=False)
     role = db.Column(db.String, nullable=False)
     key = db.Column(db.String, nullable=False)
+
+    # relationships with the other tables in the database.
     posts = db.relationship('Post', backref='author', lazy=True)
     blood_pressure = db.relationship('BloodPressure', backref='author', lazy=True)
     weight = db.relationship('Weight', backref='author', lazy=True)
@@ -44,59 +47,71 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     """Post table in database. Stores all interaction between users."""
 
+    # database columns.
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     recipient = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+
+    # foreign key for the backref int eh User table.
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 class BloodPressure(db.Model):
     """Blood pressure table in database. Stores blood pressure records for all astronauts."""
 
+    # database columns.
     id = db.Column(db.Integer, primary_key=True)
     record = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # foreign key for the backref int eh User table.
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 class Weight(db.Model):
     """Weight table in database. Stores weight records for all astronauts."""
 
+    # database columns.
     id = db.Column(db.Integer, primary_key=True)
     record = db.Column(db.String, nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    # foreign key for the backref int eh User table.
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
 def delete_user_from_db(email):
-    """Deletes user and all associated data.
+    """Deletes user and all associated data, if the user exists.
 
     Keyword args:
         email -- email of the user to be deleted.
     """
 
+    # pulls user from the database
     user = User.query.filter_by(email=email).first()
 
     if user:
+        # finds all data associated with the user in the database.
         posts_received = Post.query.filter_by(recipient=user.email).all()
-        posts = Post.query.filter_by(user_id=user.id).all()
+        posts_sent = Post.query.filter_by(user_id=user.id).all()
         blood_pressures = BloodPressure.query.filter_by(user_id=user.id).all()
         weights = Weight.query.filter_by(user_id=user.id).all()
 
+        # deletes all associated data from the database.
         for weight in weights:
             db.session.delete(weight)
 
         for blood_pressure in blood_pressures:
             db.session.delete(blood_pressure)
 
-        for post in posts:
+        for post in posts_sent:
             db.session.delete(post)
 
         for post in posts_received:
             db.session.delete(post)
 
+        # deletes user form the database and commits the changes.
         db.session.delete(user)
-
         db.session.commit()
--- ssdteam/routes.py
+++ ssdteam/routes.py
@@ -27,6 +27,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 from cryptography.fernet import Fernet
 from ssdteam import app, db, bcrypt
 from ssdteam.models import User, Post, BloodPressure, Weight, delete_user_from_db
+
 from ssdteam.forms import RegistrationForm, LoginForm, \
     PostForm, BloodPressureForm, WeightForm
 
@@ -38,31 +39,34 @@ from ssdteam.downloads import download_record
 
 @app.route("/")
 @app.route("/home")
+@login_required
 def home():
     """
     Loads home page with all post to and from the current user.
     If user isn't authenticated the n redirects to login page.
     """
 
-    if current_user.is_authenticated:
-        encrypted_posts = db.session.query(Post) \
+    # pulls encrypted posts either to or from the current user from the database.
+    encrypted_posts = db.session.query(Post) \
             .where((Post.recipient == current_user.email) | (Post.user_id == current_user.id))\
             .order_by(Post.date_posted.desc()).all()
 
-        posts = []
+    posts = []  # empty lists for decrypted posts to be appended to.
 
-        for encrypted_post in encrypted_posts:
-            post_list = [encrypted_post]
-            if post_list[0].recipient == current_user.email:
+    for encrypted_post in encrypted_posts:
+        post_list = [encrypted_post]    # decrypt() post takes a list as an argument.
+        if post_list[0].recipient == current_user.email:
+                # decrypts post using the current user's key.
                 posts.append(decrypt_post(post_list, current_user.key)[0])
 
-            else:
-                user = User.query.filter_by(email=encrypted_post.recipient).first()
-                posts.append(decrypt_post(post_list, user.key)[0])
+        else:
+            # pulls the recipient's user data form the database.
+            user = User.query.filter_by(email=encrypted_post.recipient).first()
+            # decrypts the post using the recipient's key.
+            posts.append(decrypt_post(post_list, user.key)[0])
 
-        return render_template('home.html', posts=posts, title='Home')
-
-    return redirect(url_for('login'))
+    # passes posts and title into the html template.
+    return render_template('home.html', posts=posts, title='Home')
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -74,20 +78,27 @@ def login():
     """
 
     if current_user.is_authenticated:
+        # redirects to homepage if user is already logged in.
         return redirect(url_for('home'))
 
-    form = LoginForm()
+    form = LoginForm()  # login form to be passed into the template.
     if form.validate_on_submit():
+        # if form entries are validated successfully then the user data related to
+        # the email address entered is loaded form the database.
         user = User.query.filter_by(email=form.email.data).first()
 
         if user and bcrypt.check_password_hash(user.password, form.password.data):
+            # if the user with the email address is found, and the password
+            # is checked successfully then the user is logged in.
             login_user(user)
+            # if a route other than /login was initially requested
+            # then the user is redirected to that page.
             next_page = request.args.get('next')
-
             return redirect(next_page) if next_page else redirect(url_for('home'))
 
-        flash("Login failed", 'danger')
+        flash("Login failed", 'danger')  # login failed alert.
 
+    # passes the LoginForm and page title into the html.
     return render_template('login.html', title='Login', form=form)
 
 
@@ -106,20 +117,24 @@ def new_post():
     Takes the submitted PostForm, encrypts the content and saves it to the database.
     """
 
-    form = PostForm()
+    form = PostForm()   # Post form to be passed into the template.
     if form.validate_on_submit():
-
+        # if form data is validated successfully, encrypts post content
+        # using the recipients key.
         encrypted_content = encrypt_post(form.content.data, form.recipient.data)
 
+        # Post object using the form data and the encrypted content.
         encrypted_post = Post(recipient=form.recipient.data, title=form.title.data,
                               content=encrypted_content, author=current_user)
-
+        # adds the post to the database and commits the change.
         db.session.add(encrypted_post)
         db.session.commit()
 
+        # redirects to homepage and flashes post created message.
         flash('Post created.', 'success')
         return redirect(url_for('home'))
 
+    # passes page title, PostForm, and legend into the html.
     return render_template('create_post.html', title='New Post', form=form, legend='New Post')
 
 
@@ -131,25 +146,31 @@ def blood_pressure():
     Takes data from the submitted BloodPressure form, encrypts it,
     and saves it to the database.
     """
+    if current_user.role != 'Astronaut':
+        return abort(403) # access denied if current user is not an astronaut.
 
-    form = BloodPressureForm()
+    form = BloodPressureForm()  # BloodPressure form to be passed into the template.
     if form.validate_on_submit():
+        # if form data is validated successfully, encrypts record
+        # using the users key.
         encrypted_blood_pressure = encrypt_medical_record(form.blood_pressure.data,
                                                           current_user.key)
-
+        # BloodPressure object using the form data and the encrypted content.
         bp = BloodPressure(record=encrypted_blood_pressure, user_id=current_user.id)
         db.session.add(bp)
         db.session.commit()
-
+        # redirects to blood pressure page and flashes record created message.
         flash('Blood pressure submitted.', 'success')
         return redirect(url_for('blood_pressure'))
 
+    # pulls the current user's blood pressure records from the database.
     encrypted_bp = BloodPressure.query. \
         filter_by(user_id=current_user.id).order_by(BloodPressure.date_posted.desc()).all()
-
+    # decrypts the records.
     posts = decrypt_medical_record(encrypted_bp,
                                    current_user.key)
-
+    # passes the decrypted data into the html, along with
+    # the page title, form legend, and form.
     return render_template('blood_pressure.html', title='Blood Pressure',
                            form=form, posts=posts,  legend='New Blood Pressure')
 
@@ -163,23 +184,32 @@ def weight():
     and saves it to the database.
     """
 
+    if current_user.role != 'Astronaut':
+        return abort(403) # access denied if current user is not an astronaut.
+
     form = WeightForm()
     if form.validate_on_submit():
+        # if form data is validated successfully, encrypts record
+        # using the users key.
         encrypted_weight = encrypt_medical_record(form.weight.data, current_user.key)
+        # Weight object using the form data and the encrypted content.
         weight_new = Weight(record=encrypted_weight, user_id=current_user.id)
         db.session.add(weight_new)
         db.session.commit()
-
+        # redirects to weight page and flashes record created message.
         flash('Weight submitted.', 'success')
         return redirect(url_for('weight'))
 
+    # pulls the current user's weight records from the database.
     encrypted_weight = Weight.query.filter_by(user_id=current_user.id). \
         order_by(Weight.date_posted.desc()).all()
-
+    # decrypts the records.
     posts = decrypt_medical_record(encrypted_weight,
                                    current_user.key)
-
-    return render_template('weight.html', title='Weight', posts=posts, form=form, legend='New Weight')
+    # passes the decrypted data into the html, along with
+    # the page title, form legend, and form.
+    return render_template('weight.html', title='Weight',
+                           posts=posts,form=form, legend='New Weight')
 
 
 @app.route("/astronauts")
@@ -192,19 +222,20 @@ def astronauts():
     """
 
     if current_user.role in ['Admin', 'Medic']:
-        posts = User.query.filter_by(role='Astronaut').all()
+        # pulls all users with the astronaut role from the database.
+        astronauts = User.query.filter_by(role='Astronaut').all()
+        # passes astronauts data and page title into the html.
+        return render_template('users_list.html', posts=astronauts, title='Astronauts')
 
-        return render_template('users_list.html', posts=posts, title='Astronauts')
-
-    return abort(403)
+    return abort(403)   # access denied error if current user has an incorrect role.
 
 
 @app.route("/accounts/<string:email>/bloodpressure")
 @login_required
-def astronauts_blood_pressure(email):
+def astronauts_blood_pressure(email, retunr=None):
     """
     Loads page displaying the blood pressure records of a given astronaut,
-    proving that the current user has the correct role
+    proving that the current user has the correct role, or is the astronaut.
 
     Keyword args:
         email -- email of the astronaut whose records are being viewed.
@@ -212,19 +243,23 @@ def astronauts_blood_pressure(email):
 
     if current_user.role in ['Admin', 'Medic'] \
             or current_user.email == email:
-
+        # finds the user in the database associated with the passed in email address.
         user = User.query.filter_by(email=email).first()
-        user_id = user.id
-        encrypted_bp = BloodPressure.query.\
-            filter_by(user_id=user_id).order_by(BloodPressure.date_posted.desc()).all()
+        if user:
+            # finds all the blood pressure records associated with the user.
+            encrypted_bp = BloodPressure.query.\
+                filter_by(user_id=user.id).order_by(BloodPressure.date_posted.desc()).all()
+            # decrypts the  users records.
+            records = decrypt_medical_record(encrypted_bp,
+                                           User.query.filter_by(email=email).first().key)
+            # passes records, user, and title into the html.
+            return render_template('single_medical_record.html', posts=records,
+                                   user=user, title='Blood Pressure')
 
-        posts = decrypt_medical_record(encrypted_bp,
-                                       User.query.filter_by(email=email).first().key)
+        else:
+            return abort(404)   # not found error if user not found.
 
-        return render_template('single_medical_record.html', posts=posts,
-                               user=user, title='Blood Pressure')
-
-    return abort(403)
+    return abort(403)   # access denied error if current user has an incorrect role.
 
 
 @app.route("/accounts/<string:email>/weight")
@@ -240,18 +275,23 @@ def astronauts_weight(email):
 
     if current_user.role in ['Admin', 'Medic'] \
             or current_user.email == email:
-
+        # finds the user in the database associated with the passed in email address.
         user = User.query.filter_by(email=email).first()
-        user_id = user.id
-        encrypted_weight = Weight.query.filter_by(user_id=user_id).\
-            order_by(Weight.date_posted.desc()).all()
+        if user:
+            # finds all the blood pressure records associated with the user.
+            encrypted_weight = Weight.query.filter_by(user_id=user.id).\
+                order_by(Weight.date_posted.desc()).all()
+            # decrypts the  users records.
+            posts = decrypt_medical_record(encrypted_weight,
+                                           User.query.filter_by(email=email).first().key)
+            # passes records, user, and title into the html.
+            return render_template('single_medical_record.html', posts=posts,
+                                   user=user, title='Weight')
 
-        posts = decrypt_medical_record(encrypted_weight,
-                                       User.query.filter_by(email=email).first().key)
+        else:
+            return abort(404)   # not found error if user not found.
 
-        return render_template('single_medical_record.html', posts=posts, user=user, title='Weight')
-
-    return redirect(url_for('login'))
+    return abort(403)   # access denied error if current user has an incorrect role.
 
 
 @app.route("/accounts/<string:email>/posts")
@@ -265,26 +305,31 @@ def user_posts(email):
         email -- email of the user whose posts are being viewed.
     """
 
-    if current_user.is_authenticated:
-        user = User.query.filter_by(email=email).first()
+    # finds the user in the database associated with the passed in email address.
+    user = User.query.filter_by(email=email).first()
+    if user:
+        # finds all post in the database between the current user and the user passed in.
         encrypted_posts = db.session.query(Post)\
             .where(((Post.user_id == user.id) & (Post.recipient == current_user.email))
-                   | ((Post.user_id == current_user.id) & (Post.recipient == user.email)))\
+                    | ((Post.user_id == current_user.id) & (Post.recipient == user.email)))\
             .order_by(Post.date_posted.desc()).all()
 
-        posts = []
+        posts = []  # empty list for decrypted posts to be appended to.
 
         for encrypted_post in encrypted_posts:
-            post_list = [encrypted_post]
-            if post_list[0].recipient == current_user.email:
+            post_list = [encrypted_post]    # decrypt_post() takes a list as an argument
+
+            # posts are decrypted with the appropriate key based on the recipient.
+            if encrypted_post.recipient == current_user.email:
                 posts.append(decrypt_post(post_list, current_user.key)[0])
 
-            if post_list[0].recipient == user.email:
+            if encrypted_post.recipient == user.email:
                 posts.append(decrypt_post(post_list, user.key)[0])
 
+        # passes posts, user, and title info into the html.
         return render_template('user_posts.html', posts=posts, user=user, title='Posts')
 
-    return redirect(url_for('login'))
+    return abort(404)   # not found error if user not found.
 
 
 @app.route("/accounts/<string:email>/<string:record_type>/download")
@@ -309,7 +354,7 @@ def download_data(email, record_type):
 
         return download_record(email, record_type)
 
-    return abort(403)
+    return abort(403)   # access denied error if not the data owner, or incorrect role.
 
 
 @app.route("/accounts/<string:email>", methods=['GET', 'POST'])
